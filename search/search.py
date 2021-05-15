6a4a0e4432918b11506cc334ab74bf4ea8dabc6e
Carolina Albuquerque <carolinamalbuquerque29@gmail.com>
2021-05-15 18:45:30 +0100

    Comments on code

--- search/search.py
+++ search/search.py
@@ -74,26 +74,10 @@ def tinyMazeSearch(problem):
     return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    "*** YOUR CODE HERE ***"
-    #util.raiseNotDefined()
 
     startNode = problem.getStartState()
     visited = set() # set of visited nodes
     explored = set()    # set of explored nodes
-
 
     structure = util.Stack() # stack because use LIFO implementation
 
@@ -111,8 +95,8 @@ def depthFirstSearch(problem):
             for successorNode, action, cost in problem.getSuccessors(currentNode):
                 if successorNode not in visited or successorNode not in explored:
                     explored.add(successorNode) # add sucessorNode to explored
-                    nextAction = actions.copy()
-                    nextAction.append(action)
+                    nextAction = actions.copy() # copy actions list and append the new action to achieve the goal
+                    nextAction.append(action) 
                     structure.push((successorNode, nextAction)) # add the list of actions to achieve the goal
 
 
